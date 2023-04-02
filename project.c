37040d0ec82b630d1c27337e22f6dc4d0009d000
farbubby <farhanmahbub88@gmail.com>
2023-04-02 13:11:39 -0400

    small fix

--- project.c
+++ project.c
@@ -44,7 +44,7 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 
     if (strcmp(ALUControl, "110") == 0)
     {
-        *ALUresult = B << 16;
+        B = B << 16;
     }
 
     if (strcmp(ALUControl, "111") == 0)
